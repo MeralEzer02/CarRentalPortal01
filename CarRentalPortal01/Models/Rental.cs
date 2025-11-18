74f17cda776ad6ab0dbea4cbbdd1ebe26d432566
Meral Ezer <meralezer02@gmail.com>
2025-11-18 11:58:55 +0300

    Add project files.

---
+++ CarRentalPortal01/Models/Rental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalPortal01.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        //Kiralama
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+
+        public int VehicleId { get; set; }
+
+        [ForeignKey("VehicleId")]
+        public Vehicle Vehicle { get; set; }
+    }
+}
---
+++ CarRentalPortal01/Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRentalPortal01.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string LicensePlate { get; set; }
+        public decimal DailyRentalRate { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
